c75235ccc43c1b763820c00e7ad4a144610abbef
shoryuken <shoryukeny@gmail.com>
2016-01-05 02:10:15 +0800

    init commit

---
+++ src/main/java/com/ejtone/MyService.java
@@ -0,0 +1,26 @@
+package com.ejtone;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by yuanjing on 16/1/5.
+ */
+@Component
+public class MyService implements CommandLineRunner {
+    static Log log = LogFactory.getLog(MyService.class);
+
+    @Override
+    public void run(String... strings) throws Exception {
+        try {
+            while(true) {
+                Thread.sleep(2*1000);
+                log.info("I am running...");
+            }
+        }catch (Exception ex) {
+            log.error("Error occurred.",ex);
+        }
+    }
+}
